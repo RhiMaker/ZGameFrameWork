6823f374f7a19fd3a2ecb121d342d613867e3a55
rhimaker <1791664307@qq.com>
2022-04-23 22:14:51 +0800

    1.事件中心用泛型比避免装箱拆箱 2.输入模块

--- Assets/ZGameFrameWork/Event/EventCenter.cs
+++ Assets/ZGameFrameWork/Event/EventCenter.cs
@@ -7,34 +7,87 @@ using ZGameFrameWork;
 
 namespace ZGameFrameWork
 {
+    public interface IEventInfo
+    {
+    }
+
+    public class EventInfo<T> : IEventInfo
+    {
+        public UnityAction<T> actions;
+
+        public EventInfo(UnityAction<T> action)
+        {
+            actions += action;
+        }
+    }
+
+    public class EventInfo : IEventInfo
+    {
+        public UnityAction actions;
+
+        public EventInfo(UnityAction action)
+        {
+            actions += action;
+        }
+    }
+
     public class EventCenter : BaseManager<EventCenter>
     {
-        private Dictionary<string, UnityAction<object>> eventDic = new FlexibleDictionary<string, UnityAction<object>>();
+        private Dictionary<string, IEventInfo> eventDic = new FlexibleDictionary<string, IEventInfo>();
 
-        public void AddEventListener(string name, UnityAction<object> action)
+        public void AddEventListener<T>(string name, UnityAction<T> action)
         {
             if (eventDic.ContainsKey(name))
             {
-                eventDic[name] += action;
+                (eventDic[name] as EventInfo<T>).actions += action;
             }
             else
             {
-                eventDic.Add(name, action);
+                eventDic.Add(name, new EventInfo<T>(action));
             }
         }
-
-        public void RemoveEventListener(string name, UnityAction<object> action)
+        public void AddEventListener(string name, UnityAction action)
         {
             if (eventDic.ContainsKey(name))
             {
-                eventDic[name] -= action;
+                (eventDic[name] as EventInfo).actions += action;
+            }
+            else
+            {
+                eventDic.Add(name, new EventInfo(action));
             }
         }
-        public void EventTrigger(string name,object info)
+        public void RemoveEventListener<T>(string name, UnityAction<T> action)
         {
             if (eventDic.ContainsKey(name))
             {
-                eventDic[name].Invoke(info);
+                (eventDic[name] as EventInfo<T>).actions -= action;
+            }
+        }
+        public void RemoveEventListener(string name, UnityAction action)
+        {
+            if (eventDic.ContainsKey(name))
+            {
+                (eventDic[name] as EventInfo).actions -= action;
+            }
+        }
+        public void EventTrigger<T>(string name, T info)
+        {
+            if (eventDic.ContainsKey(name))
+            {
+                if ((eventDic[name] as EventInfo<T>).actions != null)
+                {
+                    (eventDic[name] as EventInfo<T>).actions.Invoke(info);
+                }
+            }
+        }public void EventTrigger(string name)
+        {
+            if (eventDic.ContainsKey(name))
+            {
+                if ((eventDic[name] as EventInfo).actions != null)
+                {
+                    (eventDic[name] as EventInfo).actions.Invoke();
+                }
             }
         }
 
---
+++ Assets/ZGameFrameWork/Input/InputMgr.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace ZGameFrameWork
+{
+    public class InputMgr : BaseManager<InputMgr>
+    {
+        private bool isStart = false;
+        public InputMgr()
+        {
+            MonoMgr.GetInstance().AddUpdateListener(MyUpdate);
+        }
+
+        public void StartOrEndCheck(bool isOpen)
+        {
+            isStart = isOpen;
+        }
+        private void CheckKeyCode(KeyCode key)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                EventCenter.GetInstance().EventTrigger("按下", key);
+            }
+
+            if (Input.GetKeyUp(key))
+            {
+                EventCenter.GetInstance().EventTrigger("抬起", key);
+            }
+        }
+
+        private void MyUpdate()
+        {
+            if (!isStart)
+            {
+                return;
+            }
+            CheckKeyCode(KeyCode.W);
+            CheckKeyCode(KeyCode.A);
+            CheckKeyCode(KeyCode.S);
+            CheckKeyCode(KeyCode.D);
+        }
+    }
+}
